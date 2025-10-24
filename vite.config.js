d19f32657f33c9c4be1bba94e71e4b0ebee21e88
Dattatray Salunkhe <149805576+Dattatraysalunkhe@users.noreply.github.com>
2025-10-24 18:28:31 +0530

    fix:rediect issue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,18 +12,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [
-    react(),
-    // 👇 Ensure _redirects is copied to dist root
-    viteStaticCopy({
-      targets: [
-        { src: 'public/_redirects', dest: '' } // copies to dist/
-      ]
-    })
-  ],
-  build: {
-    outDir: 'dist', // Render publish directory
-  },
-  publicDir: 'public',
-
+  plugins: [react()],
 })
